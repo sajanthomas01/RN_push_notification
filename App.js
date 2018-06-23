295c87a74dcf7225b8ee78716b8ced083a0a7018
beyondinfinity01 <32977170+beyondinfinity01@users.noreply.github.com>
2018-06-23 16:38:08 +0530

    Foreground message issue fixed

--- App.js
+++ App.js
@@ -59,56 +59,66 @@ export default class Apphome extends Component{
             // store fcm token in your server
         });
         
-        this.notificationListener = FCM.on(FCMEvent.Notification, async (notif) => {
+          this.notificationListener = FCM.on(FCMEvent.Notification, async (notif) => {
             // optional, do some component related stuff
-            console.log('what i got from server in listener ',notif.custom_notification);
+            console.log('what i got from server in listener direct', notif);
+            console.log('what i got from server in listener ', notif.custom_notification);
 
-            try{
-              console.log('inside locals')
-              var data = JSON.parse(notif.custom_notification);
-              FCM.presentLocalNotification({
-                vibrate: 500,
-                title: data.title,
-                body: data.body,
-                color: data.color,
-                sub_text:data.sub_text,
-                picture: data.picture,
-                big_text: data.big_text,
-                priority: data.priority,
-                
-                large_icon: data.icon,
-                show_in_foreground: data.forground,
-               
-                
-              });
-      
+            console.log('this needs to be shown', data);
 
-            }catch(e){
-              console.log(e)
+            try {
+                console.log(notif.fcm.body)
+                FCM.presentLocalNotification({
+                    vibrate: 100,
+                    title: notif.fcm.body,
+                    body: notif.fcm.body,
+                    big_text: "Show when notification is expanded",     // Android only
+                    sub_text: notif.fcm.body,                   // Android only priority: "high",
+                    sound: "bell.mp3",
+                    large_icon: "https://image.freepik.com/free-icon/small-boy-cartoon_318-38077.jpg",
+                    show_in_foreground: true,
+                    group: 'test',
+                    targetScreen: 'detail',
+                    number: 10
+                });
+
             }
-           // console.log(JSON.parse(notif.custom_notification));
-            Alert.alert('hi')
-        });
-        
-        // initial notification contains the notification that launchs the app. If user launchs app by clicking banner, the banner notification info will be here rather than through FCM.on event
-        // sometimes Android kills activity when app goes to background, and when resume it broadcasts notification before JS is run. You can use FCM.getInitialNotification() to capture those missed events.
-        // initial notification will be triggered all the time even when open app by icon so send some action identifier when you send notification
-        FCM.getInitialNotification().then(notif => {
-           console.log('initial notif',notif)
-        });
+            catch (e) {
+                console.log(e)
+            }
 
-        FCM.presentLocalNotification({
-          vibrate: 500,
-          title: 'Hello',
-          body: 'Click me to detail page,',
-          big_text: "Show when notification is expanded",     // Android only
-          sub_text: "This is a subText",                      // Android only priority: "high",
-          sound: "bell.mp3",
-          large_icon: "https://image.freepik.com/free-icon/small-boy-cartoon_318-38077.jpg",
-          show_in_foreground: true,
-          group: 'test',
-          targetScreen: 'detail',
-          number: 10
+            try {
+                console.log('tray', notif.opened_from_tray);
+                if(notif.opened_from_tray === 1){
+                    this.props.navigation.navigate('AllNews')
+                }
+            } catch (e) {
+                console.log('tray error ', e)
+            }
+
+            try {
+                
+                var data = JSON.parse(notif.custom_notification);
+                FCM.presentLocalNotification({
+                    vibrate: 100,
+                    title: data.title,
+                    body: data.body,
+                    big_text: "Show when notification is expanded",     // Android only
+                    sub_text: data.sub_text,                      // Android only priority: "high",
+                    sound: "bell.mp3",
+                    large_icon: "https://image.freepik.com/free-icon/small-boy-cartoon_318-38077.jpg",
+                    show_in_foreground: true,
+                    group: 'test',
+                    targetScreen: 'detail',
+                    number: 10
+                });
+
+            }
+            catch (e) {
+                console.log(e)
+            }
+              
+       
         });
 
     }
